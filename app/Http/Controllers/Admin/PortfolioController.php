4fb5a9194fc591cab5128db0b4f0e98f22421c4d
Michelazani <michi.zani220@gmail.com>
2024-02-12 23:56:01 +0100

    modifiche h2 nel file admin.index

--- app/Http/Controllers/Admin/PortfolioController.php
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Portfolio;
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
@@ -12,7 +13,8 @@ class PortfolioController extends Controller
      */
     public function index()
     {
-        //
+        $portfolios=Portfolio::all();
+        return view('admin.portfolios.index', compact('portfolios'));
     }
 
     /**
